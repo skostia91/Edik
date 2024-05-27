f20bf935567698e91013fe7ab04dfba818bcddf1
Shilov Konstantin <konstantin.shilov@redcollar.ru>
2024-05-27 10:22:49 +0300

    пример

---
+++ src/main/java/com/example/edik/EdikApplication.java
@@ -0,0 +1,13 @@
+package com.example.edik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdikApplication.class, args);
+    }
+
+}
